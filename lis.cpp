230ac60f85647a4b9d0349128c4cf59bf198e36d
Pho3 <rainbowdashtheone@gmail.com>
2018-02-12 22:38:54 +0100

    night push, oh oh oohohohoh

--- lis.cpp
+++ lis.cpp
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include <cstdio>
 #include <dirent.h>
 #include <sys/types.h>
 #include <iostream>
+
+/*
+	Unusual TODO list :
+
+	- hash to check sended data ( + re sending size before parsing ?)
+	- full crypto
+	- websocket ?
+	- chunck cutting (4-sized item blocs)
+	- chat saving, history recording ?
+	- variable protection ?
+	- handshake
+	- data analysis ? ( tracker API)
+	- INI parsing lib ? (engine settings, game stats, unimportant data that could be modified)
+	- Browser backend ??? with game preview options ? Async JS with json api ?
+	- !!! AI !!!
+	- global ranking and other online stuff
+	- friends options, group chat ...
+	*/
 
 class Board {
 public:
@@ -12,6 +30,9 @@ public:
 	Board(int sz);
 	~Board();
 	void print();
+	// Modificators
+	int mmove(int px, int py, int x, int y); // Return code depending case avaible or not
+	// Generators
 	std::string gen();
 	int pak();
 	int unpak(char* fname);
@@ -31,7 +52,7 @@ Board::Board(int sz) {
 }
 
 Board::~Board() {
-		for(int i = 0; i < size; i++) delete [] board[i];
+		//for(int i = 0; i < size; i++) delete [] board[i];
 		size = 0;
 }
 
@@ -42,9 +63,49 @@ void Board::print() {
 	}
 }
 
+int Board::mmove(int px, int py, int x, int y) {
+	if(px>size || py>size || x>size || y>size) return 0;
+	int o = board[px][py];
+	int des = board[x][y];
+	int dy = py-y;
+	int dx = px-x;
+	std::cout << o << " " << des << " - " << dx << " " << dy << std::endl;
+	std::cout << "["<< px << " " << py << "] - [" << x << " " << y << "]" << std::endl;
+
+	if(px==x && py==y) return 1;
+	
+	if(o == des) {
+		if(dy == 0) {
+			des = mmove(x, y, x-dx, y);
+			board[x][y]=o;
+			board[px][py]=des;
+			return 0;
+			//return mmove(x, y, x-dx, y);
+		}
+		return 1;
+	} else {
+		if(o == 0) return 1;
+		if ((des != 0 && des != o) && (board[x-dx][y] == 0)) {
+				board[x][y]=0;
+				board[px][py]=0;
+				board[x-dx][y]=o;
+				return 0;
+			}
+		if(des == 0) {
+			board[x][y]=o;
+			board[px][py]=des;
+		}
+	}
+	
+
+	return 0;
+}
+
 int Board::unpak(char* fname) {
 	char *buffer, b1, b2;
 	buffer = new char[3];
+	int in = 0;
+	int x=  -1;
 	FILE *pfile;
 	pfile=fopen(fname,"r");
 	long pfsz = 0;
@@ -53,7 +114,7 @@ int Board::unpak(char* fname) {
 		fseek(pfile, 0, SEEK_END);
 		pfsz=ftell(pfile);
 		rewind(pfile);
-		std::cout << pfsz << std::endl;
+		//std::cout << pfsz << std::endl;
 		fread(&b1, 1, 1, pfile);
 		if(b1 != 127) {
 			std::cout << "Corrupted file ...\n";
@@ -67,10 +128,25 @@ int Board::unpak(char* fname) {
       board = new int*[size];
       for(int i = 0; i < size; i++) board[i] = new int[size];
 		}
-		std::cout << turn << " " << size << "\n";
+		//std::cout << turn << " " << size << "\n";
 		for (int i = 0; i<((((size*size)>>1)+(((size>>1)+1)<<1)))>>1; i++) {
 			fread(buffer, 2, 1, pfile);
-      std::cout << i << " ";
+			in = (int)((buffer[1] & 0x00FF)+((buffer[0] & 0x00FF) <<8) & 0xFFFF);
+			if(in==625) x++;
+			else {
+				//(board[x][y]+25*(board[x][y+1])+5*(board[x+1][y])+125*(board[x+1][y+1]));
+				//std::cout << 2*x << " " << 2*((i%5)-1) << "\t";
+				//std::cout << 2*x << "-" << 2*((i%5)-1) << " " << in%5 << "\t";
+				//std::cout << 2*x << "-" << 2*((i%5)-1)+1 << " " << in/25%5 << "\t";
+				board[2*x][2*((i%((size>>1)+1))-1)]=in%5;
+				board[2*x][2*((i%((size>>1)+1))-1)+1]=in/25%5;
+				//std::cout << 2*x+1 << "-" << 2*((i%5)-1) << " " << in/5%5 << "\t";
+				//std::cout << 2*x+1 << "-" << 2*((i%5)-1)+1 << " " << in/125%5 << "\n";
+				board[2*x+1][2*((i%((size>>1)+1))-1)]=in/5%5;
+				board[2*x+1][2*((i%((size>>1)+1))-1)+1]=in/125%5;
+				//std::cout << x << " " << (i-1)%5 << " [" << in%5 << " "<< in/5%5 << " " << in/25%5 << " " << in/125%5 << "]\n";
+			}
+     // std::cout << i << " ";
 		}
 		/*
 		fread(&buffer, 1, 1 , pfile);
@@ -96,7 +172,7 @@ int Board::unpak(char* fname) {
 
 int Board::pak() {
 	FILE *ptr_myfile;
-	ptr_myfile=fopen("test01.dat","wb");
+	ptr_myfile=fopen("saves/test01.dat","wb");
 	if (!ptr_myfile) {
 		printf("Unable to open file!");
 		fclose(ptr_myfile);
@@ -116,7 +192,7 @@ int Board::pak() {
 	for(int x=0; x < size; x+=2) {
 		for(int y=0; y<size; y+=2) {
 			i = (board[x][y]+25*(board[x][y+1])+5*(board[x+1][y])+125*(board[x+1][y+1]));
-			std::cout <<  i << " ";
+			//std::cout <<  i << " ";
 			//fwrite(((i & 0xFF00)>> 8), 1, 1, ptr_myfile);
 			//fwrite(((i & 0x00FF)), 1, 1, ptr_myfile);
 			out[p++]=(i & 0xFF00)>> 8;
@@ -129,7 +205,7 @@ int Board::pak() {
 		//std::cout << out[p-1] << out[p] << "\n";
 	}
 	for (int i=0; i<=p; i++) {
-		std::cout << (int)out[i] << " ";
+		//std::cout << (int)out[i] << " ";
 		fwrite(&out[i], 1, 1, ptr_myfile);
 	}
 //	fwrite(&out, 4+size*(size/2)+size+3, 1, ptr_myfile);
@@ -138,50 +214,11 @@ int Board::pak() {
 }
 
 std::string Board::gen() {
-	for(int j =0; j<16; j++) {
-		switch(j%4) {
-				case 0:
-					std::cout << " ";
-					break;
-				case 1:
-					std::cout << "▀";
-					break;
-				case 2:
-					std::cout << "▄";
-					break;
-				case 3:
-					std::cout << "█";
-					break;
-			}
-			switch(j>>2) {
-				case 0:
-					std::cout << " ";
-					break;
-				case 1:
-					std::cout << "▀";
-					break;
-				case 2:
-					std::cout << "▄";
-					break;
-				case 3:
-					std::cout << "█";
-					break;
-		}
-		std::cout << " " << j << " ";
-	}
-	std::cout << std::endl;
 	int i = 0;
 	std::string s;
 	for(int x=0; x < size; x+=2) {
 		for(int y=0; y<size; y+=2) {
-			i = (((board[x][y])?(1):(0))+4*((board[x][y+1])?(1):(0))+2*((board[x+1][y])?(1):(0))+8*((board[x+1][y+1])?(1):(0)))%16;
-			std::cout <<  i << " ";
-		}
-		std::cout << "\n";
-	}
-	for(int x=0; x < size; x+=2) {
-		for(int y=0; y<size; y+=2) {
-	i = (((board[x][y])?(1):(0))+4*((board[x][y+1])?(1):(0))+2*((board[x+1][y])?(1):(0))+8*((board[x+1][y+1])?(1):(0)))%16;			//std::cout <<  i << " ";
+			i = (((board[x][y])?(1):(0))+4*((board[x][y+1])?(1):(0))+2*((board[x+1][y])?(1):(0))+8*((board[x+1][y+1])?(1):(0)))%16;			//std::cout <<  i << " ";
 			switch(i%4) {
 				case 0:
 					s.append(" ");
@@ -213,7 +250,6 @@ std::string Board::gen() {
 		}
 		s.append("\n");
 	}
-	//std::cout << s << std::endl;
 	return s;
 }
 
@@ -237,7 +273,7 @@ int fwri() {
 	int counter;
 	FILE *ptr_myfile;
 	struct rec my_record;
-	ptr_myfile=fopen("test.bin","wb");
+	ptr_myfile=fopen("struct.bin","wb");
 	if (!ptr_myfile) {
 		printf("Unable to open file!");
 		return 1;
@@ -252,11 +288,29 @@ int fwri() {
 
 
 int main() {
-	Board b(8);
-	Board b2;
-	std::cout << b2.unpak("test01.dat") << std::endl;
+	int sz = 12;
+	int i = 0;
+	//std::cout << "sz: ";
+	//std::cin >> sz;
+	Board b(sz);
 	b.print();
+	b.pak();
+	Board b2;
+	b2.unpak("saves/test01.dat");
+	b2.print();
 	std::cout << b.gen();
+	b2.mmove(4, 3, 2,3);
+	b2.mmove(5, 6, 4,6);
+	b2.print();
+	int px, py, ix,iy;
+	do {
+		scanf("%d %d %d %d", &px, &py, &ix, &iy);
+		if (b2.mmove(px, py, ix,iy) == 0)
+			std::cout << b2.gen();
+		else
+			b2.print();
+	} while (1);
+	std::cout << b2.gen();
 	b.pak();
 	fwri();
 	/*
@@ -267,7 +321,9 @@ int main() {
 	//fseek ( pFile , 5 , SEEK_SET );
 	//fputs ( "0" , pFile );
 	fclose ( pFile );*/
+
 	list_dir("saves");
+
 	/*
 	pFile = fopen ("saves/test.sav" , "r");
 	if (pFile == NULL) perror ("Error opening file");
